5b57ec2ad61dda621e79dfc21502332d7e0156c3
Nacho <ibartomeus_pro2@MacBook-Pro-2.local>
2020-03-16 16:29:45 +0100

    Add flowers data

---
+++ R/flowers.R
@@ -0,0 +1,22 @@
+#' flowers
+#' 
+#' A dataset containing flower counts per transect 
+#' 
+#' \itemize{
+#'   \item SiteID: Unique name of the sites where collected. See sites table for more info.
+#'   \item Day: Day fruits were collected
+#'   \item Month: Month fruits were collected
+#'   \item Year: Year fruits were collected
+#'   \item Round: identifies of each of the rounds done to a site
+#'   \item Plot: Each of the 10 two by two meters plots where flowers where counted within a transect
+#'   \item Plant.genus: Plant genus
+#'   \item Plant.species: Plant species
+#'   \item Flower.abundance: number of flowers counted per plot
+#'   }
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name flowers
+#' @usage data(flowers)
+#' @format A data frame with X rows and 9 variables
+"flowers"
